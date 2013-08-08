48b9feb38a34e289c15cd88ccf9c9f36338f4cd8
mars <mars@a0a5f996-2bb4-401c-8778-395c3c01700f>
2013-08-08 09:58:52 +0000

    作者: mars 日期: 2013/8/8 17:52 訊息: 1. update index.html empty channel ---- 新增 : images/banner_ch_01.png , images/banner_ch_02.png , stylesheetsv3/com-9x9-cycle.css 新增 : index.html , javascript/v3/func.index.js
    git-svn-id: http://everest.teltel.com:8080/repos/misc/trunk/ui/cms@5285 a0a5f996-2bb4-401c-8778-395c3c01700f

--- javascripts/v3/func.index.js
+++ javascripts/v3/func.index.js
@@ -37,7 +37,6 @@
             options: options
         }, 'debug');
 
-        var pageId = cms.global.PAGE_ID;
         if (cms.global.USER_DATA.id) {
             $common.showProcessingOverlay();
             nn.api('GET', cms.reapi('/api/users/{userId}/channels', {
@@ -57,22 +56,19 @@
                         channel.moreImageUrl_1 = cms.config.CHANNEL_DEFAULT_IMAGE;
                         channel.moreImageUrl_2 = cms.config.CHANNEL_DEFAULT_IMAGE2;
                         channel.moreImageUrl_3 = cms.config.CHANNEL_DEFAULT_IMAGE2;
-                        if (channel.contentType === cms.config.YOUR_FAVORITE) {
-                        } else {
-                            if (channel.imageUrl && '' !== $.trim(channel.imageUrl) && channel.imageUrl !== cms.config.EPISODE_DEFAULT_IMAGE) {
-                                channel.moreImageUrl_1 = channel.imageUrl;
-                            }
-                            if (cms.config.CHANNEL_DEFAULT_IMAGE === channel.moreImageUrl_1) {
-                                channel.moreImageUrl_1 = 'images/ch_default.png';
-                            }
-                            if (channel.moreImageUrl && '' !== $.trim(channel.moreImageUrl)) {
-                                temp = channel.moreImageUrl.split('|');
-                                if (temp[0] && temp[0] !== cms.config.EPISODE_DEFAULT_IMAGE) {
-                                    channel.moreImageUrl_2 = temp[0];
-                                }
-                            }
-                            items.push(channel);
+                        if (channel.imageUrl && '' !== $.trim(channel.imageUrl) && channel.imageUrl !== cms.config.EPISODE_DEFAULT_IMAGE) {
+                            channel.moreImageUrl_1 = channel.imageUrl;
                         }
+                        if (cms.config.CHANNEL_DEFAULT_IMAGE === channel.moreImageUrl_1) {
+                            channel.moreImageUrl_1 = 'images/ch_default.png';
+                        }
+                        if (channel.moreImageUrl && '' !== $.trim(channel.moreImageUrl)) {
+                            temp = channel.moreImageUrl.split('|');
+                            if (temp[0] && temp[0] !== cms.config.EPISODE_DEFAULT_IMAGE) {
+                                channel.moreImageUrl_2 = temp[0];
+                            }
+                        }
+                        items.push(channel);
                     });
                     $('#channel-list').html('');
                     $('#channel-list-tmpl-item').tmpl(items, {
@@ -87,6 +83,25 @@
                         }
                     });
                     $('#channel-list').sortable('disable');
+                } else {
+                    $("p.order").hide();
+                    $('#content-main-wrap .constrain').empty();
+                    $('#channel-list-empty-tmpl').tmpl({
+                        id: cms.global.USER_DATA.id
+                    }).appendTo('#content-main-wrap .constrain');
+                    $('#com-9x9-cycle p.cycle-pager').html('');
+                    $('#com-9x9-cycle .wrapper ul.content').cycle({
+                        pager: '.cycle-pager',
+                        activePagerClass: 'active',
+                        updateActivePagerLink: null,
+                        fx: 'scrollHorz',
+                        speed: 1000,
+                        timeout: 6000,
+                        pagerEvent: 'mouseover',
+                        pause: 1,
+                        cleartypeNoBg: true
+                    });
+                    $('#func-nav ul li.btns').addClass("hide");
                 }
                 if (cntChannel <= 0 || (1 === cntChannel && hasFavoriteChannel)) {
                     if (!$.cookie('cms-cct')) {
