4efcda7731305612a7718393bd149b2d74943cd8
jao kuo hsuan <funnyjao@gmail.com>
2014-07-18 18:43:00 +0800

    revise pcs-landing page by bootstrop

---
+++ sass/bootstrap/javascripts/main.js
@@ -0,0 +1,10 @@
+// $(function() {
+// 	$(".sider-affix").affix({
+// 		offset: {
+// 			top: 50,
+// 			bottom: function() {
+// 				return (this.bottom = $('#comments').outerHeight(true) + $('#footer').outerHeight(true))
+// 			}
+// 		}
+// 	});
+// })
